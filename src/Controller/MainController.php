838b5a56552dd2ee6dc88a6884c649c5e0a37065
yangLENI <yang.liu2023@campus-eni.fr>
2023-07-01 01:05:25 +0200

    update

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_home', methods: ['GET'])]
+    public function home():Response{
+        return $this->render("main/home.html.twig");
+    }
+    #[Route('/test', name: 'main_home_test', methods: ['GET'])]
+    public function test():Response{
+        return $this->render("main/test.html.twig");
+    }
+}
