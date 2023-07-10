fbb09d6301a7c752e54b726fc87fe16270f2b570
Vincent.Bernier <vincent.bernier2023@campus-eni.fr>
2023-07-10 09:23:47 +0200

    create controller and twig for page 10 on 10 ('gestion des sites')

---
+++ src/Controller/AdminGestionSitesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/', name: 'admin_')]
+class AdminGestionSitesController extends AbstractController
+{
+    #[Route('/admin/gestion/sites', name: 'gestion_sites')]
+    public function index(): Response
+    {
+        return $this->render('admin_gestion_sites/index.html.twig', [
+            'controller_name' => 'AdminGestionSitesController',
+        ]);
+    }
+}
