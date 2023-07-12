42d200f1340c62a0b77b54ebd4deab6648d3142c
Yang <132666934+YangLENI@users.noreply.github.com>
2023-07-12 22:20:19 +0200

    Update Comment.php

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
@@ -15,6 +16,12 @@ class Comment
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\Length(
+        min: 2,
+        max: 150,
+        minMessage: 'Votre commentaire doit comporter au moins {{ limit }} caractères',
+        maxMessage : 'Votre commentaire ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $content = null;
 
     #[ORM\ManyToOne(inversedBy: 'comments')]
